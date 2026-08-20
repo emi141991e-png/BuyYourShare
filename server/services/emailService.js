938155a19bbe16dd31c5088ea760bcd63c55fd88
emi141991e-png <emi141991e-png@users.noreply.github.com>
2026-08-20 09:35:33 +0200

    fix: correct dataRepository import path in emailService

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import { dataRepository } from '../repository/DataRepository.js';
+import { dataRepository } from '../db/dataRepository.js';
 
 class EmailService {
   constructor() {
